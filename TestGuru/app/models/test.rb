08f38e1b8ea0911ba79f292a293905528e2523ab
crrafterr <crrafterr@test.loc>
2024-03-22 18:56:33 +0300

    Correct answers for part 4 attempt 1

--- TestGuru/app/models/test.rb
+++ TestGuru/app/models/test.rb
@@ -1,7 +1,8 @@
 class Test < ApplicationRecord
   def self.sort_tests_by_category(category_name)
-    tables = Test.joins('INNER JOIN categories ON categories.id = tests.category_id')
-
-    tables.where('categories.title = ?', category_name).order(title: :desc).pluck(:title)
+    joins('INNER JOIN categories ON categories.id = tests.category_id')
+      .where('categories.title = ?', category_name)
+      .order(title: :desc)
+      .pluck(:title)
   end
 end
