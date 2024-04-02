e0de4798d78c41643828d21d6d283f039bd7e79c
crrafterr <crrafterr@test.loc>
2024-04-02 14:30:51 +0300

    Answers for part 5

--- TestGuru/app/models/test.rb
+++ TestGuru/app/models/test.rb
@@ -1,4 +1,11 @@
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :author, class_name: 'User', foreign_key: 'author_id'
+  has_many :questions
+  has_many :results
+  has_many :users, through: :results
+
+
   def self.sort_tests_by_category(category_name)
     joins('INNER JOIN categories ON categories.id = tests.category_id')
       .where('categories.title = ?', category_name)
--- TestGuru/app/models/user.rb
+++ TestGuru/app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  has_many :results
+  has_many :tests, through: :results
+  has_many :author_for_tests, class_name: 'Test', foreign_key: :author_id
+
   def select_test_by_level(level)
     Test.joins('INNER JOIN results ON results.test_id = tests.id INNER JOIN users ON users.id = results.user_id')
         .where('users.id = ?', id)
