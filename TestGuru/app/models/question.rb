403a0f6ad6b5ff7920cabee891e8e04e7db81c6b
crrafterr <crrafterr@test.loc>
2024-04-05 14:50:27 +0300

    Answers for part 6

--- TestGuru/app/models/question.rb
+++ TestGuru/app/models/question.rb
@@ -1,5 +1,14 @@
 class Question < ApplicationRecord
   belongs_to :test
-  
+
   has_many :answers, dependent: :destroy
+
+  validates :body, presence: true
+  validate :validate_max_answers
+
+  private
+
+  def validate_max_answers
+    errors.add(:question) if question.answers.count > 4
+  end
 end
--- TestGuru/app/models/user.rb
+++ TestGuru/app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   has_many :tests, through: :results, dependent: :destroy
   has_many :author_for_tests, class_name: 'Test', foreign_key: :author_id, dependent: :destroy
 
-  def select_test_by_level(level)
-    tests.where(tests: { level: level })
-  end
+  scope :select_tests_by_level, ->(level) { where(tests: { level: level }) }
+
+  validates :name, presence: true
 end
