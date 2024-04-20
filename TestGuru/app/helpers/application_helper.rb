44048688e4f427aa8871e4f91d9c5659f9c45db0
crrafterr <crrafterr@test.loc>
2024-04-20 15:21:18 +0300

    Answers for part 8

--- TestGuru/app/helpers/application_helper.rb
+++ TestGuru/app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def year
+    Date.current.year
+  end
+
+  def github_url(author, repo)
+    link_to('TestGuru', "https://github.com/#{author}/#{repo}")
+  end
 end
--- TestGuru/app/helpers/questions_helper.rb
+++ TestGuru/app/helpers/questions_helper.rb
@@ -1,2 +1,7 @@
 module QuestionsHelper
+  def question_header(test, question)
+    return "Create New #{test.title} Question" if question.new_record?
+
+    "Edit #{test.title} Question"
+  end
 end
