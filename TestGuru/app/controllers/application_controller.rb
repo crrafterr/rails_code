1923f0edfb7cb8cfcc36a47f3d79617a71e60148
crrafterr <crrafterr@test.loc>
2024-05-14 23:02:33 +0300

    Answers for part 12

--- TestGuru/app/controllers/application_controller.rb
+++ TestGuru/app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
+
+  def default_url_options
+    { lang: I18n.locale == I18n.default_locale ? nil : I18n.locale }
+  end
+
   protected
 
   def after_sign_in_path_for(user)
     return admin_tests_path if user.is_a?(Admin)
 
     tests_path
+  end
+
+  def set_locale
+    I18n.locale = I18n.locale_available?(params[:lang]) ? params[:lang] : I18n.default_locale
   end
 end
