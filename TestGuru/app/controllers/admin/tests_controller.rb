8e9a6fa4a913eaa34c8c1e0f84a1251d70e8bcb5
crrafterr <crrafterr@test.loc>
2024-06-06 21:55:28 +0300

    Answers for part 16

--- TestGuru/app/controllers/admin/tests_controller.rb
+++ TestGuru/app/controllers/admin/tests_controller.rb
@@ -1,5 +1,6 @@
 class Admin::TestsController < Admin::BaseController
-  before_action :set_test, only: %i[show edit update destroy]
+  before_action :set_tests, only: %i[index update_inline]
+  before_action :set_test, only: %i[show edit update destroy update_inline]
 
   def show; end
 
@@ -36,6 +37,14 @@ class Admin::TestsController < Admin::BaseController
     redirect_to admin_tests_path, notice: t('.delete')
   end
 
+  def update_inline
+    if @test.update(test_params)
+      redirect_to admin_tests_path
+    else
+      render :index
+    end
+  end
+
   private
 
   def test_params
@@ -44,5 +53,9 @@ class Admin::TestsController < Admin::BaseController
 
   def set_test
     @test = Test.find(params[:id])
+  end
+
+  def set_tests
+    @tests = Test.all
   end
 end
